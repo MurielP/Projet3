d8a78221443c7a9ec0d3495a20e714bf884ba35d
MurielP <muriel.dev@bbox.fr>
2018-01-10 15:12:23 +0100

    Fichier index à jour

--- index.php
+++ index.php
@@ -2,7 +2,27 @@
 /**
  *  création de l'index 
  */
-require_once 'controller/router.php';
+session_start();
+
+//require_once 'controller/router.php';
+
+/**
+ * appel de l'autoloader pour charger les classes 
+ */
+require 'engine/autoloader.php';
+$autoloader = new Autoloader();
+
+$autoloader->addDirectories(
+	array(
+	'engine',	
+	'controller',
+	'entity',
+	'model',
+	'view',
+	'public'
+	));
+
+$autoloader->registerAutoload();
 
 $router = new Router(); 
 $router->routeQuery(); 
