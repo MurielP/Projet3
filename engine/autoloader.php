5bed073a233ecf2d58a2b15fe02e358467231473
MurielP <muriel.dev@bbox.fr>
2018-01-10 16:01:31 +0100

    autoloader ok

--- engine/autoloader.php
+++ engine/autoloader.php
@@ -18,9 +18,6 @@ class Autoloader
 				require_once $path;
 			}
 		}
-		//echo 'Essai pour charger la classe "' . $myClass . '" via la méthode "' . __METHOD__ . '()"';
-		// si namespace $myClass = str_replace('\\',DIRECTORY_SEPARATOR, $myClass ); 
-		//require_once __DIR__ . DIRECTORY_SEPARATOR . ($myClass) .'.php';
 	}
 
 	/** 
@@ -36,7 +33,35 @@ class Autoloader
 	{
 		$this->directories = (array)$directories;
 	}
-	
 }
 
 // ne pas lancer d'exception pour laisser la place au prochain autoloader
+/*class Autoloader
+{
+	/** 
+	* méthode registerAutoload enregistre l'autoload 
+	*
+	
+	public static function registerAutoload()
+	{
+		spl_autoload_extensions('.php, .class.php');
+		spl_autoload_register(array(__CLASS__, 'autoload')); 
+	}
+
+	/**
+	 * méthode autoload pour charger les fichiers 
+	 * @param  [string] $myClass [nom de la classe à charger]
+	 * @return [bool] si true enregistre l'autoloader
+	 
+	public static function autoload($myClass)
+	{
+		echo 'Chargement de la classe "' . $myClass . '" via la méthode "' . __METHOD__ . '()"';
+
+		$myClass = strtolower($myClass);
+
+		// si namespace : $myClass = str_replace('\\',DIRECTORY_SEPARATOR, $myClass ); 
+		require_once __DIR__ .'/'. $myClass .'.php';
+	}
+}
+*/
+
