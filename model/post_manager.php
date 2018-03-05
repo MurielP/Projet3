3f791d47c690d410bb1c7db5dcf277dec8195fcf
MurielP <muriel.dev@bbox.fr>
2018-03-05 12:14:14 +0100

    Modification de la fonction getPost()pour compter les lignes retournées par la requête SELECT

--- model/post_manager.php
+++ model/post_manager.php
@@ -14,7 +14,7 @@ class Post_manager extends Database
 		$sql = ('SELECT id, author, title, content, DATE_FORMAT(creation_date,\'%d/%m/%Y à %Hh%imin%ss\') AS formatted_creation_date FROM posts ORDER BY creation_date DESC');
 		$posts = $this->executeQuery($sql);
 		
-		// tableau vide
+		// tableau vide de tout billet
 		$postsObj = array();
         foreach ($posts as $post){
             $postObj = new Post($post);
@@ -30,6 +30,33 @@ class Post_manager extends Database
 	}
 
 	/**
+	 * [getPost récupère un billet selon son id]
+	 * @param  [int] $post_id [id du billet]
+	 * @return [un billet]
+	 */
+	 public function getPost($post_id) {
+	    	
+	    	$sql = ('SELECT COUNT(*)  FROM  posts WHERE id = ?'); 
+
+			if($req = $this->executeQuery($sql, array($post_id))) {
+
+				/* Récupère le nombre de lignes qui correspond à la requête SELECT */
+				if($req->fetchColumn() > 0){
+
+					 /* Effectue la vraie requête SELECT et travaille sur le résultat */
+					$sql = ('SELECT id, author, title, content, DATE_FORMAT(creation_date,\'%d/%m/%Y à %Hh%imin%ss\') AS formatted_creation_date FROM posts WHERE id = ?');
+					foreach ($req = $this->executeQuery($sql, array($post_id)) as $row){
+						$result = new Post($row);
+						return $result;	
+					}
+				}	
+			/* Aucune ligne ne correspond -- faire quelque chose d'autre */		
+			} else {
+				throw new Exception('Aucun billet ne correspond au numéro ' .$post_id. '.');
+			}
+	   	}
+
+    /**
 	 * [countPosts compte le nombre d'articles publiés]
 	 * @return [int] [nbr d'articles]
 	 */
@@ -39,27 +66,7 @@ class Post_manager extends Database
 		$req = $this->executeQuery($sql);
 		$result = $req->fetchColumn();
 		return $result;
-		
 	}
-	/**
-	 * [getPost récupère un billet selon son id avec une requête préparée]
-	 * @param  [int] $post_id [id du billet]
-	 * @return affiche un billet 
-	 */
-	
-	public function getPost($post_id) {
-        $sql = ('SELECT id, author, title, content, DATE_FORMAT(creation_date,\'%d/%m/%Y à %Hh%imin%ss\') AS formatted_creation_date FROM posts WHERE id = ?');
-		$post = $this->executeQuery($sql, array($post_id));
-
-		// rowCount() retourne le nbr de ligne affectées par le dernier appel à la fonction execute() -> si ds $post j'ai un post_id alors je vais afficher le billet
-		if ($post->rowCount() > 0) {
-			// fetch() = va chercher la 1ère ligne de résultat
-			$result = new Post($post->fetch());
-			return $result;
-		}
-		else 
-			throw new Exception('Aucun billet ne correspond au numéro ' .$post_id. '.');
-    } 
 
     public function savePost($lastPost)
 	{
@@ -129,23 +136,26 @@ class Post_manager extends Database
     	}
     }
 
-   
-
-	
-    /*
-    public function getPost($post_id) {
-    	
-    	$sql = ('SELECT COUNT(*) AS nb_id FROM  posts WHERE id = ?'); 
-		$req = $this->executeQuery($sql, array([$id]));
-		$resultPost = $req->fetchColumn();
-		//print($resultPost);
-				if ($resultPost > 0) {
-				$result = new Post($post_id);
-				return $result;	
-		} else {
-			throw new Exception('Aucun billet ne correspond au numéro ' .$post_id. '.');
+/*
+    /**
+	 * [getPost récupère un billet selon son id avec une requête préparée]
+	 * @param  [int] $post_id [id du billet]
+	 * @return  un billet 
+	 */
+/*
+	public function getPost($post_id) {
+        $sql = ('SELECT id, author, title, content, DATE_FORMAT(creation_date,\'%d/%m/%Y à %Hh%imin%ss\') AS formatted_creation_date FROM posts WHERE id = ?');
+		$post = $this->executeQuery($sql, array($post_id));
+		// rowCount() retourne le nbr de ligne affectées par le dernier appel à la fonction execute() -> si ds $post j'ai un post_id alors je vais afficher le billet
+		if ($post->rowCount() > 0) {
+			// fetch() = va chercher la 1ère ligne de résultat
+			$result = new Post($post->fetch());
+			return $result;
 		}
-   	}	
-   	*/
+		else 
+			throw new Exception('Aucun billet ne correspond au numéro ' .$post_id. '.');
+    } 
+*/
+   	
 }
  
