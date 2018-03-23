55418208995a133aaf794ac14e29ecc33cb86c9b
MurielP <muriel.dev@bbox.fr>
2018-03-23 10:24:41 +0100

    Modification du menu pour le gérer seulement depuis le template.php - si admin connecté un menu personnalisé sinon menu visiteur

--- view/template.php
+++ view/template.php
@@ -41,6 +41,32 @@
     <body class="<?php if($this->getAdmin()) { echo 'admin';} ;?>">
 <!-- Header -->
     <header id="header" class="masthead text-center">
+        <nav class="navbar navbar-expand-lg bg-secondary fixed-top navbar-shrink" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="index.php"><img src="public/img/begins_Simon_Migaj_pexels.jpg" class="rounded" width="30"  height="30" alt="Let's begin image d'accueil" title="Que l'aventure commence !"> Jean Forteroche</a>
+
+                <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu<i class="fa fa-bars"></i></button>
+
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <?php if(isset($_SESSION['adminUsername'])) { ?>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="index.php">Accueil</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="index.php?action=logAdmin">Tableau de bord</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="index.php?action=logoutAdmin">Me déconnecter</a></li>
+                    <?php  } else { ?>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="index.php">Accueil</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="index.php?action=logAdmin">Espace administrateur</a></li>
+                     <?php } ?> 
+                             
+             <!-- Préparation de l'espace membre
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded " href="index.php?action=registerUser">Devenir membre</a>
+            </li>
+            -->
+            </ul>
+        </div>
+    </div>
+</nav>
         <div class="carousel-item">
             <img class="img-fluid w-100 d-none d-sm-block mx-auto" src="public/img/snow_pexels.jpg" alt="Let's begin image d'accueil" title="Que l'aventure commence !">
             <div class="carousel-caption">
--- view/view_home.php
+++ view/view_home.php
@@ -1,32 +1,4 @@
-<nav class="navbar navbar-expand-lg bg-secondary fixed-top navbar-shrink" id="mainNav">
-      <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="index.php"><img src="public/img/begins_Simon_Migaj_pexels.jpg" class="rounded" width="30"  height="30" alt="Let's begin image d'accueil" title="Que l'aventure commence !"> Jean Forteroche</a>
-
-        <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Menu
-          <i class="fa fa-bars"></i>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item mx-0 mx-lg-1">
-              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="index.php?action=logAdmin">Espace administrateur</a>
-            </li>
-    		
-    		 <!--
-            <li class="nav-item mx-0 mx-lg-1">
-              <a class="nav-link py-3 px-0 px-lg-3 rounded " href="index.php?action=registerUser">Devenir membre</a>
-            </li>
-            	<li class="nav-item mx-0 mx-lg-1">
-              <a class="nav-link py-3 px-0 px-lg-3 rounded " href="#index.php?action=logUser">Espace membre</a>
-            </li>
-     		-->
-            </ul>
-        </div>
-    </div>
-</nav>
- 
- <div id="mainContainer" class="container">
+<div id="mainContainer" class="container">
 
 <?php
 //var_dump($_SESSION['success']);
